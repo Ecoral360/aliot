00dd4687d70c905d6a37c50aade6d8a6457e0ed2
Mathis Laroche <ecomath360@gmail.com>
2021-09-30 14:21:45 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+if __name__ == "__main__":
+    setup(name='aliot',
+          author=['Mathis Laroche', 'Enric Soldevila'],
+          version='0.1.0',
+          description='python IOT library compatible with the ALIVE ecosystem',
+          packages=find_packages(
+              include=['aliot.aliot', 'aliot.aliot.*']),
+          install_requires=[
+              'msgpack',
+              'schedule',
+              'websocket'
+          ],
+          setup_requires=['flake8', 'autopep8']
+          )
